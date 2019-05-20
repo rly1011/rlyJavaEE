8a127b8bb966e18da5ae62ee2b9069324877a8c1
rly101112 <rly101112@163.com>
2019-05-20 10:11:17 +0800

    工程结构大调整

--- rlyServlet/src/com/renly/servlet/LoginServlet.java
+++ rlyServlet/src/com/renly/servlet/LoginServlet.java
@@ -2,14 +2,13 @@ package com.renly.servlet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
 /**
  * @author ${任岚杨}
  * @title: LoginServlet
---
+++ rlyServlet/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rly10
+  Date: 2019/5/17
+  Time: 9:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>任岚杨的第一个JSP</title>
+  </head>
+  <body>
+  <h1>这是动态的页面，会被翻译成Servlet！</h1>
+  </body>
+</html>
